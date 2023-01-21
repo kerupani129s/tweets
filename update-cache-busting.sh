d230d7b2d51c18c6b08ef8a7196409bb453e0fe3
ケルパニ＠猫 <32389989+kerupani129s@users.noreply.github.com>
2023-01-21 15:47:51 +0900

    add: 「ノスタルジアの鍵盤」のツイートまとめを追加

---
+++ update-cache-busting.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+set -euoo pipefail posix
+cd "$(dirname "$0")"
+
+# 
+function openssl_md4() {
+	# OpenSSL 3.0 & OpenSSL 1.1
+	openssl md4 -provider legacy "$@" 2>/dev/null || openssl md4 "$@"
+}
+
+function content_hash() {
+	local -r file="$1"
+	openssl_md4 "$file" | awk '{ print substr($NF, 0, 20) }'
+}
+
+# 
+SITE_CSS_PARAM="v=$(content_hash ./docs/site.css)"
+readonly SITE_CSS_PARAM
+
+# 
+sed -Ei \
+	-e 's/(["/]site\.css\?)[^"]*/\1'"$SITE_CSS_PARAM"'/g' \
+	./docs/index.html \
+	./docs/keyboard-of-nostalgia/index.html
+
+# 
+echo 'OK'
