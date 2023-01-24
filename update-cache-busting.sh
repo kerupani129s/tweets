1caf00d64374c8480585291a76e136806525c462
ケルパニ＠猫 <32389989+kerupani129s@users.noreply.github.com>
2023-01-24 19:47:21 +0900

    add: 「みんなのリズム天国」のツイートまとめを追加

--- update-cache-busting.sh
+++ update-cache-busting.sh
@@ -21,6 +21,8 @@ readonly SITE_CSS_PARAM
 sed -Ei \
 	-e 's/(["/]site\.css\?)[^"]*/\1'"$SITE_CSS_PARAM"'/g' \
 	./docs/index.html \
+	./docs/games/index.html \
+	./docs/games/rhythm-heaven-fever/index.html \
 	./docs/computer/index.html \
 	./docs/computer/my-third-homebuilt-computer/index.html \
 	./docs/music-game-controllers/index.html \
